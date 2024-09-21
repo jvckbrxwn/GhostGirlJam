b63133ca531d46619e55d8331272b6f2adf286eb
Stanislav Kokoshko <stanislav.kokoshko@whinsygames.co>
2024-09-21 15:43:34 +0300

    BaseInteractable add

---
+++ Assets/Scripts/InteractableObjects/BaseInteractableComponent.cs
@@ -0,0 +1,52 @@
+using Managers;
+using ServiceLocator;
+using UnityEngine;
+
+namespace InteractableObjects.Base
+{
+	public abstract class BaseInteractableComponent : MonoBehaviour, IInteractable
+	{
+		protected PlayerManager playerManager;
+		protected TooltipManager tooltipManager;
+		protected InventoryManager inventoryManager;
+
+		protected bool canInteract = false;
+
+		protected void Start()
+		{
+			playerManager = ServiceManager.Instance.GetManager<PlayerManager>();
+			tooltipManager = ServiceManager.Instance.GetManager<TooltipManager>();
+			inventoryManager = ServiceManager.Instance.GetManager<InventoryManager>();
+		}
+
+		private void Update()
+		{
+			if (canInteract && Input.GetKeyDown(KeyCode.E))
+			{
+				playerManager.InteractWith(this);
+				tooltipManager.HideUseTooltip();
+				canInteract = false;
+			}
+		}
+
+		public abstract void Interact();
+
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.CompareTag(playerManager.PlayerTag))
+			{
+				canInteract = true;
+				tooltipManager.ShowUseTooltip(transform, other.transform, 1);
+			}
+		}
+		
+		private void OnTriggerExit2D(Collider2D other)
+		{
+			if (other.CompareTag(playerManager.PlayerTag))
+			{
+				canInteract = false;
+				tooltipManager.HideUseTooltip();
+			}
+		}
+	}
+}
--- Assets/Scripts/NPC/NPCController.cs
+++ Assets/Scripts/NPC/NPCController.cs
@@ -1,44 +1,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using InteractableObjects.Base;
-using Managers;
 using Objects.Room.PickUpObjects;
-using ServiceLocator;
 using Spine.Unity;
 using UnityEngine;
 
 namespace Objects.Room.NPC
 {
-	public class NPCController : MonoBehaviour, IInteractable
+	public class NPCController : BaseInteractableComponent
 	{
 		[SerializeField] private SkeletonAnimation skeletonAnimation;
 		[SerializeField] private List<PickUpType> pickUpItems;
 		[field: SerializeField] public bool FinishedQuest { get; private set; } = false;
 
-		private PlayerManager playerManager;
-		private TooltipManager tooltipManager;
-		private InventoryManager inventoryManager;
-		private bool canInteract = false;
-
-		private void Start()
-		{
-			playerManager = ServiceManager.Instance.GetManager<PlayerManager>();
-			tooltipManager = ServiceManager.Instance.GetManager<TooltipManager>();
-			inventoryManager = ServiceManager.Instance.GetManager<InventoryManager>();
-			//skeletonAnimation.AnimationState.SetAnimation(0, "idle", true);
-		}
-
-		private void Update()
-		{
-			if (canInteract && Input.GetKeyDown(KeyCode.E))
-			{
-				playerManager.InteractWith(this);
-				tooltipManager.HideUseTooltip();
-				canInteract = false;
-			}
-		}
-
-		public void Interact()
+		public override void Interact()
 		{
 			if (FinishedQuest)
 			{
@@ -62,24 +37,6 @@ namespace Objects.Room.NPC
 				FinishedQuest = true;
 				Debug.Log("Quest was finished");
 				Debug.Log("Show tooltip with love");
-			}
-		}
-
-		private void OnTriggerEnter2D(Collider2D other)
-		{
-			if (other.CompareTag(playerManager.PlayerTag))
-			{
-				canInteract = true;
-				tooltipManager.ShowUseTooltip(transform, other.transform, 0.5f);
-			}
-		}
-
-		private void OnTriggerExit2D(Collider2D other)
-		{
-			if (other.CompareTag(playerManager.PlayerTag))
-			{
-				canInteract = false;
-				tooltipManager.HideUseTooltip();
 			}
 		}
 	}
